2fd6aeac6fb4b204df3c00d9083023e98b1bbed9
giovana <giovanalarocca07@gmail.com>
2025-05-08 09:45:40 -0300

    criando o javascript do projeto

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+//DECLARANDO O ARRAY DE IMAGENS
+
+let imagens=[
+    'src/assets/imagem1.jpg',
+    'src/assets/imagem2.jpg',
+    'src/assets/imagem3.jpg',
+];
+
+//DECLARANDO AS VARIAVEIS
+
+let index=0;
+let tempo=3000;
+
+//CRIANDO A FUNÃO SLIDE SHOW
+
+function slideshow(){
+    document.getElementById("image").src=imagens[index]
+    index++
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout("slideshow()",tempo)
+}
+slideshow();
